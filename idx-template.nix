915f8fce2d821af917b31f34c5ee09efaa073c70
Rody Davis <rody.davis.jr@gmail.com>
2024-09-05 16:18:23 -0700

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -4,8 +4,11 @@
         pkgs.git
     ];
     bootstrap = ''
-        cp -r ${./dev}/* "$out"
+        cp -r ${./}/* "$out"
         mkdir "$out"/.idx
+        rm "$out"/dev.nix
+        rm "$out"/idx-template.nix
+        rm "$out"/idx-template.json
         cp ${./dev.nix} "$out"/.idx/dev.nix
         install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
         chmod -R u+w "$out"
