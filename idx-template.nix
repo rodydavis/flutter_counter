ad3ec503895b0e3dfe33608ea284c3fc70f12d68
Rody Davis <rody.davis.jr@gmail.com>
2024-09-05 17:04:19 -0700

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -4,7 +4,7 @@
         pkgs.git
     ];
     bootstrap = ''
-        shopt -s dotglob; cp -r !"$out" "$out"/
+        shopt -s dotglob; cp -r * "$out"/
         # rm "$out"/dev.nix
         # rm "$out"/idx-template.nix
         # rm "$out"/idx-template.json
